17f7f41222b0366bf47fee13d4b2f651e4b10c8e
Cyrillus <chenetcyril@free.fr>
2022-09-13 12:00:43 +0200

    v1.2

--- src/Middlewares/getSearch.js
+++ src/Middlewares/getSearch.js
@@ -4,7 +4,7 @@ const getSearch = (store) => (next) => (action) => {
     if (action.type === 'GET_SEARCH') {
         const term = store.getState().termSearch;
         axios.get(`https://api.bing.microsoft.com/v7.0/search?q=${term}`, {
-            headers:  { 'Ocp-Apim-Subscription-Key': '42a77d5fb7f24a65b9b7e94e82248b50' },
+            headers:  { 'Ocp-Apim-Subscription-Key': process.env.REACT_APP_MY_KEY },
           })
             .then((response) => {
                 store.dispatch({
