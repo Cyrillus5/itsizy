61d94b81be13418682dbb49eea41d5b493069b49
Cyrillus <chenetcyril@free.fr>
2022-11-28 14:57:11 +0100

    v1.4

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -15,10 +15,12 @@ function App() {
   useEffect(() => {
     if(!selectedTheme) {
       document.body.style.backgroundImage = 'url(http://www.camilleperrin.fr/wp-content/uploads/2019/01/tumblr_pkpkt0qnhB1x7np1ao1_1280.jpg)';
-      document.body.style.backgroundSize = "auto"
+      document.body.style.backgroundSize = "auto";
+      document.body.style.backgroundAttachment = "fixed";
     } else {
       document.body.style.backgroundImage = 'url(https://i.pinimg.com/originals/e3/41/22/e34122e017c4f91836f1b6d9a83f4429.jpg)';
-      document.body.style.backgroundSize = "auto"
+      document.body.style.backgroundSize = "auto";
+      document.body.style.backgroundAttachment = "fixed";
     }
   });
 
